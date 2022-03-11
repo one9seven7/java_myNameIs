c55dd91ecec5b5cddb2e00414b346d6e556d04fc
Piotr Nykiel <99175611+one9seven7@users.noreply.github.com>
2022-03-11 14:52:02 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Piotr";
+        String surname = "Nykiel";
+        String email = "nykipio@gmail.com";
+        int age = 45;
+
+        System.out.println("My name is " + name + ", my surname is " + surname +
+                " , my email adress is " + email + " and I'm " + age + " years old.");
+    }
+}
+
